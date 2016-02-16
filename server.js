14bf6b04734c4fee31ecaa191cfb1b88e9d368cc
ericj376 <ericellingson376@gmail.com>
2016-02-16 14:06:27 -0700

    added bears router

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals');
 
+var Bear = require('./models/bears');
+
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
@@ -12,9 +15,37 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next){
+	console.log('Something is happening');
+	next();
+});
+
 router.get('/', function(req, res){
 	res.json({ message: 'hooray! welcome to our api!'});
 });
+
+router.route('/bears')
+	.post(function(req, res){
+		
+		var bear = new Bear();
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+		
+		bear.save(function(err, bear){
+			if(err){
+			console.log(err)
+			} else {
+			res.json(bear)
+
+			}
+		})	
+	})
+
+.get(function(req, res){
+
+	res.json({title: 'what the hell am I doing?'})
+})
 
 app.use('/api', router);
 
